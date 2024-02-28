248dd42b7677087c5d3b1814e822e456348cfff8
Joao <joaodslourenco@gmail.com>
2024-02-28 15:01:25 -0300

    feat: add test for empty file

--- file_system_store.go
+++ file_system_store.go
@@ -37,8 +37,30 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 	f.database.Encode(f.league)
 }
 
-func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+func initialisePlayerDBFile(file *os.File) error {
 	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
+}
+
+func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+	err := initialisePlayerDBFile(file)
+
+	if err != nil {
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
+	}
+
 	league, err := NewLeague(file)
 
 	if err != nil {
